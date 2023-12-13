fa3ce77dc703f55b1372ea5339f1eb14b059ad55
庆坤 宋 <1325249191@qq.com>
2023-12-13 19:35:06 +0800

    2023/12/13 变换

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -30,7 +30,8 @@ public:
 		m_CameraPosition(0.0f),
 		m_CameraMovedSpeed(1.0f),
 		m_CameraRotation(0.0f),
-		m_CameraRotationSpeed(100.0f)
+		m_CameraRotationSpeed(100.0f),
+		m_SquarePosition(0.0f)
 	{
 		m_VertexArray.reset(Hazel::VertexArray::Create());
 
@@ -59,10 +60,10 @@ public:
 		m_SquareVA.reset(Hazel::VertexArray::Create());
 
 		float squareVertices[3 * 4] = {
-			-0.75f,-0.75f,0.0f,
-			0.75f,-0.75f,0.0f,
-			0.75f,0.75f,0.0f,
-			-0.75f,0.75f,0.0f
+			-0.5f,-0.5f,0.0f,
+			0.5f,-0.5f,0.0f,
+			0.5f,0.5f,0.0f,
+			-0.5f,0.5f,0.0f
 		};
 
 		std::shared_ptr<Hazel::VertexBuffer> SquareVB;
@@ -85,13 +86,14 @@ public:
 		layout(location = 0) in vec3 a_Position;
 		layout(location = 1) in vec4 a_Color;
 		uniform mat4 u_ViewProjection;
+		uniform mat4 u_Transform;
 		out vec3 v_Position;
 		out vec4 v_Color;
 		void main()
 		{
 			v_Position = a_Position;
 			v_Color = a_Color;
-			gl_Position = u_ViewProjection*vec4(a_Position,1.0);
+			gl_Position = u_ViewProjection*u_Transform*vec4(a_Position,1.0);
 		}
 		)";
 		std::string fragmentSrc = R"(
@@ -111,11 +113,12 @@ public:
 		#version 460 core
 		layout(location = 0) in vec3 a_Position;
 		uniform mat4 u_ViewProjection;
+		uniform mat4 u_Transform;
 		out vec3 v_Position;
 		void main()
 		{
 			v_Position = a_Position;
-			gl_Position = u_ViewProjection*vec4(a_Position,1.0);
+			gl_Position = u_ViewProjection*u_Transform*vec4(a_Position,1.0);
 		}
 		)";
 		std::string fragmentSrc2 = R"(
@@ -164,7 +167,18 @@ public:
 		m_Camera.SetRotation(m_CameraRotation);
 
 		Hazel::Renderer::BeginScene(m_Camera);
-		Hazel::Renderer::Submit(m_Shader2, m_SquareVA);
+
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for (int i = 0; i < 20; i++)
+		{
+			for (int j = 0; j < 20; j++)
+			{
+				glm::vec3 pos(i * 0.11f, j * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				Hazel::Renderer::Submit(m_Shader2, m_SquareVA, transform);
+			}
+		}
 		Hazel::Renderer::Submit(m_Shader, m_VertexArray);
 		Hazel::Renderer::EndScene();
 	}
@@ -193,6 +207,8 @@ private:
 	float m_CameraRotation;
 	float m_CameraMovedSpeed;
 	float m_CameraRotationSpeed;
+
+	glm::vec3 m_SquarePosition;
 };
 
 class Sandbox :public Hazel::Application
